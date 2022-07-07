63696e43ad58d9e17841d2c03b1bc1a67adddb71
vvtrnv <swatik07@yandex.ru>
2022-07-07 12:33:03 +0700

    Preparation

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 // Include gulp
 var gulp = require('gulp'),
   // Include Our Plugins
-  less = require('gulp-less'),
+  less = require('gulp-css'),
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer');
 
 gulp.task('css', function () {
   gulp
-    .src('./less/main.less')
+    .src('./css/main.css')
     .pipe(less({ strictMath: true }))
     .pipe(postcss([
       autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
@@ -16,7 +16,7 @@ gulp.task('css', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['css']);
+  gulp.watch('./css/**/*.css', ['css']);
 });
 
 gulp.task('default', ['css', 'watch']);
